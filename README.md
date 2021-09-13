f83be65960fb0f59cdf88a9d1d70b92667739dca
Yongxuan Zhang <44033547+Yongxuanzhang@users.noreply.github.com>
2021-09-13 18:58:54 -0400

    feat: init commit (#1)
    * feat: init commit
    
    * docs: update readme
    
    * test: add ci
    
    * test: add ci
    
    * chore: updates

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# executor-doc2query
+# Doc2QueryExecutor
+
+**Doc2QueryExecutor** is an executor which generates "questions" from sentences and add them into chunks.
+
+
+## Usage 
+
+```python
+from jina import Flow, DocumentArray, Document
+
+document_array= DocumentArray(
+        [
+            Document(id='doc1',content='Hello world'),
+            Document(id='doc2',content='Neural search is deep neural network-powered information retrieval'),
+        ])
+
+f = Flow().add(uses='jinahub://Doc2QueryExecutor', uses_with={'num_questions': 10})
+
+with f:
+    resp = f.post(on='/', inputs=document_array, return_results=True)
+    print(f'{resp[0].data.docs[0].chunks}')
+
+```
+
+
+## Reference
+- See the [Document Expansion by Query Prediction](https://github.com/castorini/docTTTTTquery)
---
+++ executor.py
@@ -0,0 +1,43 @@
+from typing import Iterable
+
+from jina import DocumentArray, Executor, requests, Document
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+class Doc2QueryExecutor(Executor):
+    """
+    An executor which generates "questions" from sentences and add them into chunks.
+    
+    """
+    def __init__(self,
+                 num_questions: int = 10,
+                 default_traversal_paths: Iterable[str] = ('r',),
+                 **kwargs):
+        super().__init__(**kwargs)
+        """
+        :param num_questions: the number of questions to generate
+        :param default_traversal_paths: the traverse path on docs, e.g. ['r'], ['c']
+        """
+
+        self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self._tokenizer = T5Tokenizer.from_pretrained('castorini/doc2query-t5-base-msmarco')
+        self._model = T5ForConditionalGeneration.from_pretrained('castorini/doc2query-t5-base-msmarco')
+        self._model.to(self._device)
+        self._num_questions = num_questions
+        self._default_traversal_paths = default_traversal_paths
+
+    @requests
+    def doc2query(self, docs: DocumentArray, **kwargs):
+        for d in docs.traverse_flat(self._default_traversal_paths):
+            input_ids = self._tokenizer.encode(d.content, return_tensors='pt').to(self._device)
+
+            outputs = self._model.generate(
+                input_ids=input_ids,
+                max_length=64,
+                do_sample=True,
+                top_k=self._num_questions,
+                num_return_sequences=self._num_questions)
+
+            for o in outputs:
+                d.chunks.append(Document(content=self._tokenizer.decode(o, skip_special_tokens=True)))

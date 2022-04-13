f46da6c76947e769489e6a2b5584a743df4c99d3
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-04-13 09:35:07 +0100

    feat: migrate to 3.0

--- executor.py
+++ executor.py
@@ -1,43 +1,49 @@
-from typing import Iterable
-
-from jina import DocumentArray, Executor, requests, Document
 import torch
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from docarray import Document, DocumentArray
+from jina import Executor, requests
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 
 class Doc2QueryExecutor(Executor):
     """
     An executor which generates "questions" from sentences and add them into chunks.
-    
+
     """
-    def __init__(self,
-                 num_questions: int = 10,
-                 traversal_paths: Iterable[str] = ('r',),
-                 **kwargs):
+
+    def __init__(self, num_questions: int = 10, traversal_paths: str = '@r', **kwargs):
         """
         :param num_questions: the number of questions to generate
-        :param default_traversal_paths: the traverse path on docs, e.g. ['r'], ['c']
+        :param default_traversal_paths: the traverse path on docs, e.g. '@r', '@c'
         """
         super().__init__(**kwargs)
 
         self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self._tokenizer = T5Tokenizer.from_pretrained('castorini/doc2query-t5-base-msmarco')
-        self._model = T5ForConditionalGeneration.from_pretrained('castorini/doc2query-t5-base-msmarco')
+        self._tokenizer = T5Tokenizer.from_pretrained(
+            'castorini/doc2query-t5-base-msmarco'
+        )
+        self._model = T5ForConditionalGeneration.from_pretrained(
+            'castorini/doc2query-t5-base-msmarco'
+        )
         self._model.to(self._device)
         self._num_questions = num_questions
         self._traversal_paths = traversal_paths
 
     @requests
     def doc2query(self, docs: DocumentArray, **kwargs):
-        for d in docs.traverse_flat(self._traversal_paths):
-            input_ids = self._tokenizer.encode(d.content, return_tensors='pt').to(self._device)
+        for d in docs[self._traversal_paths]:
+            input_ids = self._tokenizer.encode(d.text, return_tensors='pt').to(
+                self._device
+            )
 
             outputs = self._model.generate(
                 input_ids=input_ids,
                 max_length=64,
                 do_sample=True,
                 top_k=self._num_questions,
-                num_return_sequences=self._num_questions)
+                num_return_sequences=self._num_questions,
+            )
 
             for o in outputs:
-                d.chunks.append(Document(content=self._tokenizer.decode(o, skip_special_tokens=True)))
+                d.chunks.append(
+                    Document(text=self._tokenizer.decode(o, skip_special_tokens=True))
+                )
--- tests/test_doc2query.py
+++ tests/test_doc2query.py
@@ -1,5 +1,6 @@
 import pytest
-from jina import Document, DocumentArray, Flow
+from docarray import Document, DocumentArray
+from jina import Flow
 
 from executor import Doc2QueryExecutor
 
@@ -10,9 +11,14 @@ NUM_QUESTIONS = 10
 def docs():
     return DocumentArray(
         [
-            Document(id='doc1',
-                     content='Jina is a neural search framework that allows anyone to build SOTA & scalable deep learning search applications in minutes.'),
-            Document(id='doc2', content=' neural search is deep neural network-powered information retrieval'),
+            Document(
+                id='doc1',
+                text='Jina is a neural search framework that allows anyone to build SOTA & scalable deep learning search applications in minutes.',
+            ),
+            Document(
+                id='doc2',
+                text=' neural search is deep neural network-powered information retrieval',
+            ),
         ]
     )
 
@@ -28,10 +34,8 @@ def test_flow(docs):
     f = Flow().add(uses=Doc2QueryExecutor)
 
     with f:
-        resp = f.post(
+        result_docs = f.post(
             on='/',
             inputs=docs,
-            return_results=True,
         )
-        print(f'{resp}')
-        assert len(resp[0].docs[0].chunks) == 10
+        assert len(result_docs[0].chunks) == 10
